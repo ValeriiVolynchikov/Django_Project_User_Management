e9ff7e465543c055c24df86090e3c5b3b5cae6a8
valerii volynchikov <avto162@gmail.com>
2025-06-14 08:28:53 +0300

    модификация и миграция

--- config/settings.py
+++ config/settings.py
@@ -116,9 +116,42 @@ os.environ["SSL_CERT_FILE"] = certifi.where()
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.yandex.ru"
-EMAIL_PORT = 465
-EMAIL_USE_TLS = False
-EMAIL_USE_SSL = True
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+# EMAIL_USE_SSL = True
 EMAIL_HOST_USER = "valeriy.volynchikov@yandex.ru"
-EMAIL_HOST_PASSWORD = os.getenv("YA_PASS")
+# EMAIL_HOST_PASSWORD = os.getenv("YA_PASS")
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+SERVER_EMAIL = EMAIL_HOST_USER
+
+CACHE_ENABLED = True
+if CACHE_ENABLED:
+    CACHES = {
+        "default": {
+            "BACKEND": "django.core.cache.backends.redis.RedisCache",
+            "LOCATION": "redis://redis:6379/1",
+        }
+    }
+
+# # В settings.py
+# LOGGING = {
+#     'version': 1,
+#     'disable_existing_loggers': False,
+#     'handlers': {
+#         'console': {
+#             'class': 'logging.StreamHandler',
+#         },
+#         'file': {
+#             'class': 'logging.FileHandler',
+#             'filename': 'debug.log',
+#         },
+#     },
+#     'loggers': {
+#         'django': {
+#             'handlers': ['console', 'file'],
+#             'level': 'DEBUG',
+#             'propagate': True,
+#         },
+#     },
+# }
--- users/views.py
+++ users/views.py
@@ -1,9 +1,16 @@
-from django.contrib.auth.views import PasswordResetView, PasswordResetConfirmView, PasswordResetDoneView, \
-    PasswordResetCompleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.models import Group
+from django.contrib.auth.views import (PasswordResetCompleteView,
+                                       PasswordResetConfirmView,
+                                       PasswordResetDoneView,
+                                       PasswordResetView)
+from django.core.mail import send_mail
+from django.http import HttpResponseForbidden
 from django.urls import reverse_lazy
-from django.views.generic import CreateView, UpdateView
+from django.views.generic import CreateView, ListView, TemplateView, UpdateView
 
-from users.forms import UserRegisterForm, UserProfileForm
+from config.settings import EMAIL_HOST_USER
+from users.forms import UserProfileForm, UserRegisterForm
 from users.models import CustomUser
 
 
@@ -11,9 +18,40 @@ from users.models import CustomUser
 class RegisterView(CreateView):
     form_class = UserRegisterForm
     template_name = "users/register.html"
-    success_url = reverse_lazy("mailing_service:mailing_list")
+    success_url = reverse_lazy("users:email_confirmation")
 
-class ProfileView(UpdateView):
+    def form_valid(self, form):
+        user = form.save(commit=False)
+        user.is_active = False  # Отключаем возможность логина
+        user.generate_token()  # Генерируем токен подтверждения
+        user.save()  # Теперь сохраняем в БД
+
+        group = Group.objects.get(name="Пользователи")
+        user.groups.add(group)
+        verification_url = (
+            f"http://{self.request.get_host()}/users/email-confirm/{user.token}/"
+        )
+        send_mail(
+            subject="Подтверждение почты",
+            message=f"Здравствуйте, перейдите по ссылке для подтверждения почты: {verification_url}",
+            from_email=EMAIL_HOST_USER,
+            recipient_list=[user.email],
+        )
+
+        return super().form_valid(form)
+
+
+class EmailConfirmationView(TemplateView):
+    model = CustomUser
+    template_name = "users/email_confirmation.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["title"] = "Письмо активации отправлено"
+        return context
+
+
+class ProfileView(LoginRequiredMixin, UpdateView):
     model = CustomUser
     form_class = UserProfileForm
     template_name = "users/profile.html"
@@ -21,6 +59,29 @@ class ProfileView(UpdateView):
 
     def get_object(self, **kwargs):
         return self.request.user
+
+
+class UsersListView(LoginRequiredMixin, ListView):
+    model = CustomUser
+    template_name = "users/users_list.html"
+
+    def dispatch(self, request, *args, **kwargs):
+        # Проверяем, имеет ли пользователь право на просмотр списка клиентов
+        if not request.user.has_perm("users.view_customuser"):
+            return HttpResponseForbidden(
+                "У вас нет прав для просмотра списка пользователей."
+            )
+        return super().dispatch(request, *args, **kwargs)
+
+    def get_queryset(self):
+        # Получаем группу "Пользователи"
+        users_group = Group.objects.get(name="Пользователи")
+
+        # Получаем всех пользователей из этой группы, кроме текущего пользователя
+        queryset = CustomUser.objects.filter(groups=users_group).exclude(
+            id=self.request.user.id
+        )
+        return queryset
 
 
 class CustomPasswordResetView(PasswordResetView):
@@ -36,7 +97,6 @@ class CustomPasswordResetDoneView(PasswordResetDoneView):
 class CustomPasswordResetConfirmView(PasswordResetConfirmView):
     success_url = reverse_lazy("users:password_reset_complete")
     template_name = "users/password_reset_confirm.html"  # Указываем свой шаблон
-
 
 
 class CustomPasswordResetCompleteView(PasswordResetCompleteView):
